8ff827c4fdaa2eef5979ae00ad669fc75bffc921
LZAH <laith43d@gmail.com>
2017-04-14 18:37:40 +0300

    final product

--- src/components/NewsList.vue
+++ src/components/NewsList.vue
@@ -1,6 +1,23 @@
 <template>
     <div class="newslist">
-        NewsList
+        <div class="container">
+            <ul class="media-list">
+                <li class="media" v-for="article in articles">
+                    <div class="media-left">
+                        <a v-bind:href="article.url" target="_blank">
+                            <img class="media-object" v-bind:src="article.urlToImage">
+                        </a>
+                    </div>
+                    <div class="media-body">
+                        <h4 class="media-heading">
+                            <a v-bind:href="article.url" target="_blank">{{article.title}}</a>
+                        </h4>
+                        <h5 v-if="article.author"><i>by {{article.author}}</i></h5>
+                        <p>{{article.description}}</p>
+                    </div>
+                </li>
+            </ul>
+        </div>
     </div>
 </template>
 
@@ -8,11 +25,39 @@
 <script>
     export default {
         name: 'newslist',
-
+        data () {
+            return{
+                articles: [],
+            }
+        },
+        props: ['source'],
+        watch: {
+            source: function (val) {
+                this.updateSource(val)
+            }
+        },
+//        created: function () {
+//            this.updateSource(this.source)
+//        },
+        methods: {
+            updateSource: function (source) {
+                this.$http.get('https://newsapi.org/v1/articles?source=' + source + '&apiKey=917b585fa2da4c23b15e171a43bbdf18')
+                    .then(response => {
+                        this.articles = response.data.articles;
+                    })
+            }
+        }
 
     }
 </script>
 
 <style scoped>
-
+.media-object {
+    width: 128px;
+    padding: 10px;
+}
+    .media {
+        border-top: 1px solid lightgrey;
+        padding-top: 10px;
+    }
 </style>
--- src/components/SourceSelection.vue
+++ src/components/SourceSelection.vue
@@ -1,9 +1,16 @@
 <template>
     <div class="sourceselection">
-        <h4>Please select news source:</h4>
-        <select class="form-control" @change="sourceChanged">
-            <option v-bind:value="source.id" v-for="source in sources">{{source.name}}</option>
-        </select>
+        <div class="jumbotron">
+            <h2><span class="glyphicon glyphicon-list-alt"></span> News Sources Selector</h2>
+            <h4>Please select news source:</h4>
+            <select class="form-control" @change="sourceChanged">
+                <option v-bind:value="source.id" v-for="source in sources">{{source.name}}</option>
+            </select>
+        <div v-if="source">
+            <h6>{{source.description}}</h6>
+            <a v-bind:href="source.url" class="btn btn-primary" target="_blank">go to {{source.name}} website</a>
+        </div>
+        </div>
     </div>
 </template>
 
@@ -29,12 +36,14 @@
                         this.source = this.sources[i]
                     }
                 }
+                this.$emit('sourceChanged', e.target.value)
             }
         },
         created: function () {
             this.$http.get('https://newsapi.org/v1/sources?language=en')
                 .then(response => {
                     this.sources = response.data.sources;
+                    this.source = this.sources[0]
                 })
         }
 
